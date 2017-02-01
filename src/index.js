d59c1459073a9eb301c9e3dc652bc124d9eb262e
Alejandro Oviedo <alexoviedo999@gmail.com>
2017-01-31 22:31:03 -0600

    added redux, action, reducers

--- src/index.js
+++ src/index.js
@@ -1,57 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SearchBar from './components/SearchBar.js';
-import GifList from './components/GifList.js';
-import GifModal from './components/GifModal.js'
-import request from 'superagent';
-import './styles/app.css';
+import App from './containers/App';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
 
-class App extends React.Component {
+const store = configureStore();
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      gifs: [],
-      selectedGif: null,
-      modalIsOpen: false
-    }
-  }
-
-  openModal = (gif) => {
-    this.setState({
-      selectedGif: gif,
-      modalIsOpen: true
-    })
-  }
-
-  closeModal = () => {
-    this.setState({
-      selectedGif: null,
-      modalIsOpen: false
-    })
-  }
-
-  handleTermChange = (term) => {
-    const url = `http://api.giphy.com/v1/gifs/search?q=${term.replace(/\s/g, '+')}&api_key=dc6zaTOxFJmzC`;
-
-    request.get(url, (err, res) => {
-      this.setState({ gifs: res.body.data })
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <SearchBar onTermChange={this.handleTermChange} />
-        <GifList gifs={this.state.gifs}
-          onGifSelect={selectedGif => this.openModal(selectedGif) } />
-          <GifModal modalIsOpen={this.state.modalIsOpen}
-            selectedGif={this.state.selectedGif}
-            onRequestClose={ () => this.closeModal() } />
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+);
